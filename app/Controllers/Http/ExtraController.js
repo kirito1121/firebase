068a3d90b75bde7a651ea76059d1f631f083903f
Kiet Ngo <kietngo@azwebplus.com>
2019-01-28 11:52:04 +0700

    Lưu dữ liệu Lưu dữ liệu lên firebase cho brand, store,service,extra

--- app/Controllers/Http/ExtraController.js
+++ app/Controllers/Http/ExtraController.js
@@ -1,6 +1,30 @@
-'use strict'
-
+"use strict";
+const FirebaseAdmin = use("FirebaseAdmin");
 class ExtraController {
+  async store({ request, response }) {
+    try {
+      let { name, slug, id } = request.all();
+      let db = FirebaseAdmin.firestore();
+      let ref = db.collection("extras").doc(`${slug}-${id}`);
+      id = parseInt(id);
+      await ref.set({ name, slug, id });
+      response.json({ success: true });
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async update({ params, request, response }) {
+    try {
+      let { name, slug } = request.all();
+      let db = FirebaseAdmin.firestore();
+      let ref = db.collection("extras").doc(`${slug}-${params.id}`);
+      await ref.update({ name, slug });
+      response.json({ success: true });
+    } catch (error) {
+      return error;
+    }
+  }
 }
 
-module.exports = ExtraController
+module.exports = ExtraController;
