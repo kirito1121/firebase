33143c2405bb4dea2b50f5c4e2e246ffdd9f31be
Kiet Ngo <kietngo@azwebplus.com>
2019-01-29 10:53:21 +0700

    Get Service group service

--- public/js/mainfirebase.js
+++ public/js/mainfirebase.js
@@ -1,21 +1,22 @@
-$(function () {
-  getService()
-
+$(function() {
+  getService();
 });
-
 
 function getService() {
   if (firebase) {
     var db = firebase.firestore();
-    db.collection("services").get().then(snapshot => {
-      snapshot.forEach(doc => {
-        console.log(doc.data())
-            const html = `<li class="main-nav-list"><a href="#">` +
-              doc.data().name +
-              `<span class="number"></span></a></li>`;
+    db.collection("serviceGroups")
+      .get()
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+          console.log(doc.data());
+          const html =
+            `<li class="main-nav-list"><a href="#">` +
+            doc.data().name +
+            `<span class="number"></span></a></li>`;
 
-            $('#listmenu').append(html);
-      })
-    })
+          $("#listmenu").append(html);
+        });
+      });
   }
 }
